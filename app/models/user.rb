578c47f1976d7a2087b5a19ae1ee45e0161e0d31
reeseglasscock <reesegpdx@gmail.com>
2018-10-01 13:04:11 -0700

    add relationship to projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          validates :email, presence: true
          validates :password, presence: true
          validates :email, uniqueness: true
+  has_and_belongs_to_many :projects
 end
