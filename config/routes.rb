28fa1e885c6e16754de47a5c769e4865b214d4ae
reeseglasscock <reesegpdx@gmail.com>
2018-10-01 14:33:11 -0700

    add projects routes and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'projects#index'
   devise_for :users
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
