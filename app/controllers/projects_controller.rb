dd932d2ad200d9a8842c4972a384bb734e8abafc
reeseglasscock <reesegpdx@gmail.com>
2018-10-01 18:03:54 -0700

    add actions for index, new, create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,27 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @user_projects = current_user.projects
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @user = current_user
+    if @project.save
+      @user.projects << @project
+      flash[:success] = "New Project Created"
+      redirect_to '/projects'
+    else
+      flash[:error] = "Please check again"
+      redirect_to '/projects/new'
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
