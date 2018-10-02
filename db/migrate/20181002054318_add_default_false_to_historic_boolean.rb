de3d407f9ec8ed4a5280fb25af7fc56635bb8cf7
reeseglasscock <reesegpdx@gmail.com>
2018-10-01 22:48:36 -0700

    add default false for historic value for projects_users

---
+++ db/migrate/20181002054318_add_default_false_to_historic_boolean.rb
@@ -0,0 +1,5 @@
+class AddDefaultFalseToHistoricBoolean < ActiveRecord::Migration[5.2]
+  def change
+    change_column :projects_users, :historic, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_051756) do
+ActiveRecord::Schema.define(version: 2018_10_02_054318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_051756) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "historic"
+    t.boolean "historic", default: false
     t.index ["project_id", "user_id"], name: "index_projects_users_on_project_id_and_user_id"
     t.index ["user_id", "project_id"], name: "index_projects_users_on_user_id_and_project_id"
   end
