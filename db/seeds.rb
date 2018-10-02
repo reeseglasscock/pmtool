87850483a1802901192af2d6969eb62a1071533f
reeseglasscock <reesegpdx@gmail.com>
2018-10-01 19:31:31 -0700

    add seed file to create users, projects, and assigne users to projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = User.destroy_all
+    seed = Project.destroy_all
+    seed = Seed.new
+    seed.generate_users
+    seed.generate_projects
+    seed.add_users_to_projects
+  end
+
+  def generate_projects
+    200.times do
+      Project.create!(title: Faker::Commerce.department, description: Faker::Lorem.sentence(25))
+    end
+  end
+
+  def generate_users
+    20.times do
+      @name = Faker::Name.name
+      User.create!(name: @name, email: Faker::Internet.free_email(@name), password: 'password')
+    end
+  end
+
+  def add_users_to_projects
+    500.times do
+      @project = Project.all.shuffle.first
+      @user = User.all.shuffle.first
+      @user.projects << @project
+    end
+  end
+end
+
+Seed.begin
