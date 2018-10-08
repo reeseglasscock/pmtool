6691d39a075b26ed8c85cdc3ef8698d994a48b12
reeseglasscock <reesegpdx@gmail.com>
2018-10-08 16:46:48 -0700

    remove jquery for semantic flash that is no longer being used

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,13 +1,5 @@
 $(document).on("turbolinks:load", function () {
 
-  $('.message .close')
-  .on('click', function () {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-      ;
-  });
-
   $('.ui.dropdown')
     .dropdown()
     ; 
