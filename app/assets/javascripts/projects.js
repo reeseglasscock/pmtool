f9822c04eb92ad34921c96177b3bc8ea40b6da4e
reeseglasscock <reesegpdx@gmail.com>
2018-10-09 15:54:28 -0700

    move event listeners to js file to load with page, remove from AJAX

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load", function () {
-
+console.log("Reached JS File")
   $('.ui.dropdown')
     .dropdown()
     ; 
@@ -16,5 +16,44 @@ $(document).on("turbolinks:load", function () {
       }
     })
     ;
-
+  $("a#edit_project").on('click', function(event) {
+    event.preventDefault()
+    show_edit_project()
+  })
+  $("a#members").on('click', function(event) {
+    event.preventDefault()
+    show_project_members()
+  })
+  $("a#posts").on('click', function(event) {
+    event.preventDefault()
+    show_project_posts()
+  })
 });
+
+function show_edit_project() {
+  console.log("Reached Show Edit Project")
+  $('a#edit_project').addClass('active')
+  $('a#members').removeClass('active')
+  $('a#posts').removeClass('active')
+  $(".display_add_user_form").show();
+  $(".display_project_posts").hide();
+  $(".display_remove_members_form").hide();
+};
+
+function show_project_members() {
+  $('a#members').addClass('active')
+  $('a#edit_project').removeClass('active')
+  $('a#posts').removeClass('active')
+  $(".display_add_user_form").hide();
+  $(".display_project_posts").hide();
+  $(".display_remove_members_form").show();
+}
+
+function show_project_posts() {
+  $('a#members').addClass('active')
+  $('a#edit_project').removeClass('active')
+  $('a#posts').removeClass('active')
+  $(".display_add_user_form").hide();
+  $(".display_project_posts").show();
+  $(".display_remove_members_form").hide();
+}
