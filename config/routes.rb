aa59ba0d313ce52ffe5099c213b117ec1f5fa438
reeseglasscock <reesegpdx@gmail.com>
2018-10-14 14:58:29 -0700

    add spacing for readability for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@
 
 Rails.application.routes.draw do
   root to: 'projects#index'
+
   devise_for :users
+
   resources :projects do
+    resources :posts
     delete 'user', to: 'projects#destroy_user'
   end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
