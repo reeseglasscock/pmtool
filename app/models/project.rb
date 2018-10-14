2ef0bf69dc3bb88961413dcf0ba95ebed1708754
reeseglasscock <reesegpdx@gmail.com>
2018-10-14 14:58:02 -0700

    add inverse of to save time for searching through memory

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@
 
 class Project < ApplicationRecord
   has_many :projects_users
-  has_many :posts
+  has_many :posts, inverse_of: :project
   has_many :users, through: :projects_users
   validates :title, presence: true
   validates :description, presence: true
