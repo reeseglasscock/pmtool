b914d94a8cc2d77f7f81eb8f56478935ad3025a7
reeseglasscock <reesegpdx@gmail.com>
2018-10-15 14:14:03 -0700

    create callback for removing associations of user after they delete account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_destroy :remove_assigned_projects_and_created_posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
@@ -12,4 +13,9 @@ class User < ApplicationRecord
   has_many :projects_users
   has_many :projects, through: :projects_users
   has_many :posts
+
+  def remove_assigned_projects_and_created_posts
+    Post.where(user_id: self.id).destroy_all
+    ProjectsUser.where(user_id: self.id).destroy_all
+  end
 end
