a15cbc7c0a58f739d13869acb231033f71fba846
reeseglasscock <reesegpdx@gmail.com>
2018-10-16 20:02:12 -0700

    add active record model associations to post_comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :project, inverse_of: :posts
+  has_many :post_comments, inverse_of: :post_comments
   validates :comment, :title, presence: true
 
   scope :created_at, -> { order('created_at DESC')}
