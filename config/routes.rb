1dc2e87b6480cdd720b69e47f95881b186c8e307
reeseglasscock <reesegpdx@gmail.com>
2018-10-21 12:14:24 -0700

    move post_comments routes outside of projects

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   end
 
   resources :post_comments, only: %i[new create destroy]
-
-  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
