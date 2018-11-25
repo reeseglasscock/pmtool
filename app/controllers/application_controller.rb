a1d50c52e397c78652220527a9410887f4dfa232
reeseglasscock <reesegpdx@gmail.com>
2018-11-25 14:56:56 -0800

    add user authentication using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
