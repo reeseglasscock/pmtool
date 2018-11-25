92cd227566da1293de9c18d1ee876b166c74338d
reeseglasscock <reesegpdx@gmail.com>
2018-11-24 18:54:06 -0800

    change devise sign in and sign out  routes to be more ui friendly

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+devise_for :users, skip: [:sessions], controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  as :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  # devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   resources :projects do
     delete 'user', to: 'projects#destroy_user'
