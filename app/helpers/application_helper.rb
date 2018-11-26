c6d288efb7664c0cb7a2d0ed7a51ced652693af0
reeseglasscock <reesegpdx@gmail.com>
2018-11-25 22:08:42 -0800

    set up action cable for chat room messaging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-end
+  def gravatar_for(user, opts = {})
+    opts[:alt] = user.name
+    if user.profile_picture?
+      image_tag "#{user.profile_picture}", opts
+    else
+      image_tag "https://robohash.org/#{user.email}", opts
+    end
+  end
+end 
