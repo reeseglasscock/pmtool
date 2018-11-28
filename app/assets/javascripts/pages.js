3ad17147e1df53274efd8bfaa72e5561075b50d8
reeseglasscock <reesegpdx@gmail.com>
2018-11-28 15:37:00 -0800

    add javascript form validation for login

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on("turbolinks:load", function () {
+
+  $('form.log_in')
+    .form({
+      fields: {
+        email: {
+          identifier: 'user[email]',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter a email'
+            }
+          ]
+        },
+        password: {
+          identifier: 'user[password]',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please add a password'
+            }
+          ]
+        }
+      }
+    });
+
+  })
