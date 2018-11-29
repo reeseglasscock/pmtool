7e8272a942a2443effce74c01b5bb817d04459a1
reeseglasscock <reesegpdx@gmail.com>
2018-11-28 17:00:57 -0800

    change error type to error to display red error message for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ protected
   private
 
   def user_not_authorized
-    flash[:warning] = "You are not authorized to perform this action."
+    flash[:error] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
 end
