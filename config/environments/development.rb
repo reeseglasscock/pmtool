f7a10b7c5f79eef132847124d5d864847e68239a
reeseglasscock <reesegpdx@gmail.com>
2018-12-08 17:42:20 -0800

    user letter opener for development mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'teamsuitemanager.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :sparkpost
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
