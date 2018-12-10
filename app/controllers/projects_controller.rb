bae8ebdb3a45fe33c031f1209f8dfcd3d0bc6a11
reeseglasscock <reesegpdx@gmail.com>
2018-12-10 12:03:09 -0800

    add params permitted for created a project with a due date

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -71,7 +71,7 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:title, :description)
+    params.require(:project).permit(:title, :description, :due_date)
   end
 
   def page_number
